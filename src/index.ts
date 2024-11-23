8ca20cfa7e665cbad69038d288b11fd1dbdbb62f
Serohman <NA>
2024-11-22 19:49:48 -0800

    fix: Fix linting errors

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,3 @@
 export function greet(name: string): string {
   return `Hello, ${name}!`;
 }
-
-const foo: any = "me";
-console.log(foo);
-
-// TODO
-// 3. Add readme.md (Must have so that it's useful for others)
