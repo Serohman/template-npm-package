6d3d1de0960d9f4f068649325e2faf6c8f0cf9f1
Seroh <141598689+Serohman@users.noreply.github.com>
2024-11-22 18:35:07 -0800

    fix(githooks): Move npm audit outside lint-staged

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,8 @@ export function greet(name: string): string {
   return `Hello, ${name}!`;
 }
 
-console.log("hi");
+const foo: any = "me";
+console.log(foo);
+
+// TODO
+// 3. Add readme.md (Must have so that it's useful for others)
